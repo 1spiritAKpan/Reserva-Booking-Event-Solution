601994b1ba9911f2c062b2d59f513082bb60f165
maranatha <danielanie@gmail.com>
2024-07-01 08:05:39 +0100

    make new changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -76,5 +76,46 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+window.onload = function () {
+  let slideIndex = 0;
+  showSlide();
+  
+}
 
+window.onload = function () {
+  let slideIndex = 0;
+  showSlides();
 
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("testimonal-header-img");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+  }
+
+  window.currentSlide = function (n) {
+    let slides = document.getElementsByClassName("testimonal-header-img");
+    let dots = document.getElementsByClassName("dot");
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[n - 1].style.display = "block";
+    dots[n - 1].className += " active";
+    slideIndex = n;
+  };
+};
